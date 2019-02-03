3bf179b56b7fdf575b40a13e1d06290ef83bddc4
Satoru SATOH <satoru.satoh@gmail.com>
2019-02-03 11:50:08 +0900

    add

---
+++ lint.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+set -ex
+cd ${0%/*}
+
+#playbooks=${1:-*.yml}
+#inventory=inventory/hosts
+#ansible_lint="$(which ansible-lint-3 2>/dev/null || which ansible-lint 2>/dev/null)"
+
+yamllint --strict $(find . -type f | grep -E '.yml$')
+
+#for p in ${playbooks:?}; do
+#        ${ansible_lint:?} -vvv $p -x ANSIBLE0006
+#        ansible-playbook -i ${inventory:?} -vvv $p --syntax-check
+#done
+
