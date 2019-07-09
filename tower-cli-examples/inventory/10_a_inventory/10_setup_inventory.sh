232fa21b5d695835bc8f15c5ab73ac442a2f5f73
Satoru SATOH <satoru.satoh@gmail.com>
2019-07-09 15:37:14 +0900

    fix: some bugs

--- tower-cli-examples/inventory/10_a_inventory/10_setup_inventory.sh
+++ tower-cli-examples/inventory/10_a_inventory/10_setup_inventory.sh
@@ -14,12 +14,13 @@ tower-cli project create --name test_vars_project --organization Default \
   --monitor --scm-type git --scm-url https://github.com/ssato/awx-playground
 
 # Inventories
-tower-cli inventory_source create --name local_inv_src --organization Default \
+tower-cli inventory create --name local_inv --organization Default
+tower-cli inventory_source create --name local_inv_src \
   --source scm --source-path hosts --source-project test_vars_project \
-  --overwrite true --update-on-launch true --inventory local_inv_src
+  --overwrite true --update-on-launch true --inventory local_inv
 
 # Job templates
-tower-cli job_template create --name test_vars --inventory local_inv_src \
+tower-cli job_template create --name test_vars --inventory local_inv \
   --project test_vars_project --playbook playbooks/test_vars.yml
 
 # vim:sw=2:ts=2:et:
