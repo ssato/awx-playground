b4e71aa05a794c3a748c0e3b43d877bccab673dc
Satoru SATOH <satoru.satoh@gmail.com>
2019-07-18 17:55:33 +0900

    implement some test cases

--- roles/test_java/molecule/default/tests/test_default.py
+++ roles/test_java/molecule/default/tests/test_default.py
@@ -1,3 +1,8 @@
+# References:
+# - https://testinfra.readthedocs.io
+# - https://github.com/dwalleck/testinfra-by-example/
+#
+
 import os.path
 import os
 import testinfra.utils.ansible_runner
@@ -10,3 +15,19 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 def test_facts(host):
     vars = host.ansible.get_variables()
     assert vars
+
+    assert host.exists("java")
+
+    # a.
+    jvc_s = "java -version"
+    jvc = host.run(jvc_s)
+
+    assert jvc.rc == 0
+    assert jvc.stdout == ''
+    assert jvc.stderr
+    assert "1.8" in jvc.stderr
+
+    # b.
+    host.run_expect([0], jvc_s)
+
+# vim:et:
