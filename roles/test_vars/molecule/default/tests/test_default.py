59dde72f56fd77a519663330397e8e8377e14002
Satoru SATOH <satoru.satoh@gmail.com>
2019-07-18 17:33:21 +0900

    change: restructure the role, test_vars

--- roles/test_vars/molecule/default/tests/test_default.py
+++ roles/test_vars/molecule/default/tests/test_default.py
@@ -1,15 +1,14 @@
-import os.path
 import os
 import testinfra.utils.ansible_runner
-
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_facts(host):
-    vars = host.ansible.get_variables()
-    assert vars
-    assert vars.get('a', False)
-    assert vars.get('b', False)
-    assert vars.get('c', False)
+def test_vars(host):
+    vars = host.ansible("debug", "var=vars_")
+
+    # ..seealso:: roles/test_vars/defaults/main.yml
+    assert vars.get('a', None) == 0, repr(vars)
+    assert vars.get('b', False) == 'bbb', repr(vars)
+    assert vars.get('c', False) == [1, 2, 3]
